190d9d25c88dc876732904974d81e5f4bcd0cbd7
Jxnfernandx <orangutangklan@gmail.com>
2023-10-04 19:07:13 -0500

    Updated Nav and responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const link = document.getElementById("link");
+document.querySelectorAll('nav a')
+  .forEach(e => e.addEventListener('click', function () {
+    const dataId = e.dataset.id;
+    change(dataId);
+    // Add text color change to the clicked link
+    document.querySelectorAll('nav a').forEach(a => a.style.color = '');
+    e.style.color = 'red'; // Change the color of the clicked link to red
+  }));
 
-link.addEventListener("click", () => {
-    link.style.color = "red";
-})
+function change(n) {
+  let panels = document.querySelectorAll('main div')
+  panels.forEach(p => p.classList.remove('active'))
+  panels[n - 1].classList.add('active')
+}
